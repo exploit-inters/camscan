02a1bc214d2d703d4042d205fcfb7cb1fc6465d2
Gornostay <36516130+gornostay25@users.noreply.github.com>
2018-04-14 12:33:20 +0300

    Update camscan.py

--- camscan.py
+++ camscan.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python3
+import socket
+import subprocess as sp
+import os
+import sys
+import random
+import time as t
+try:
+	from netaddr import IPNetwork
+	import requests
+except ModuleNotFoundError:
+	print("будласка встановіть бібліотеки\npip install -r requirements.txt\n")
+	exit()
 
 
+	
+
+	
+	
+	
+	
 def main():
 	pass
-
-
-
-
 if __name__ == "__main__":
 	main()
