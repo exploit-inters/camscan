8979d31835efdb21fdcfdfab140a799f682565e8
Gornostay <36516130+gornostay25@users.noreply.github.com>
2018-04-15 15:32:23 +0300

    Update install.py

--- install.py
+++ install.py
@@ -7,3 +7,12 @@ sp.call('pip install -r requirements.txt',shell=True)
 sp.call('rm -fr requirements.txt',shell=True) 
 sp.call('rm -fr install.py',shell=True)
 os.system('rm -fr README.md .git/ ')
+print('install finished!\n')
+print('start program? (y or n)')
+a = input().upper()
+if a = 'Y':
+  os.system('python3 camscan.py')
+elif a = 'N':
+  exit()
+exit()
+
