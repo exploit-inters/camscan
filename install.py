a37d0710601cf3046f49e2219318d5f1d673916f
gornostay25 <volodia.palamar250405@gmail.com>
2018-04-15 15:35:43 +0300

    add

--- install.py
+++ install.py
@@ -1,6 +1,9 @@
+#!/usr/bin/python3
 import subprocess as sp
 import os
 
-os.system('rm -fr README.md .git/ ')
-sp.call('pip install -r requirements.txt',shell=True)
 
+sp.call('pip install -r requirements.txt',shell=True) 
+sp.call('rm -fr requirements.txt',shell=True) 
+sp.call('rm -fr install.py',shell=True)
+os.system('rm -fr README.md .git/ ')
