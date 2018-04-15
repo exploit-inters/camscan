863ff271f49d260e6900093a348d406cd7aacb4f
gornostay25 <volodia.palamar250405@gmail.com>
2018-04-15 17:15:13 +0300

    add

--- install.py
+++ install.py
@@ -1,18 +1,9 @@
 #!/usr/bin/python3
 import subprocess as sp
 import os
-
-
 sp.call('pip install -r requirements.txt',shell=True) 
 sp.call('rm -fr requirements.txt',shell=True) 
 sp.call('rm -fr install.py',shell=True)
 os.system('rm -fr README.md .git/ ')
 print('install finished!\n')
-a = input('start program? (y or n): ')
-a.upper()
-if a == 'Y':
-  os.system('python3 camscan.py')
-elif a == 'N':
-  exit()
-print('123')
-
+print('now you can start program')
