4fa2008d6a7e9fd77e065b50e543c8bad7961d00
Gornostay <36516130+gornostay25@users.noreply.github.com>
2018-04-15 18:10:07 +0300

    Update ipscaner.py

--- lib/ipscaner.py
+++ lib/ipscaner.py
@@ -1,7 +1,19 @@
 #!/usr/bin/python3
-def scan(ip, port):
-	return False
-
+def scan(ip,p):
+	host = str(ip)
+	port = int(p)
+	timeout = 5
+	args = socket.getaddrinfo(host, port, socket.AF_INET, socket.SOCK_STREAM)
+	for family, socktype, proto, canonname, sockaddr in args:
+		s = socket.socket(family, socktype, proto)
+		socket.setdefaulttimeout(timeout)
+		try:
+			s.connect(sockaddr)
+		except:
+			return False
+		else:
+			s.close()
+			return True
 
 if __name__ == '__main__':
 	exit()
