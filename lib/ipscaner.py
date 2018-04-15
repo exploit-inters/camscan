3b43a54fd406b0a1f18e7f57b7d2435f3ff0b12f
Gornostay <36516130+gornostay25@users.noreply.github.com>
2018-04-15 18:27:55 +0300

    Update ipscaner.py

--- lib/ipscaner.py
+++ lib/ipscaner.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
 import socket
+import subprocess as sp
+import os
+import sys
+import random
+import time as t
+try:
+	from netaddr import *
+	import requests
+except ModuleNotFoundError:
+	print("будласка встановіть бібліотеки\npip install -r requirements.txt\n")
+	exit()
 def scan(ip,p):
 	host = str(ip)
 	port = int(p)
