5129376693356fc9510083195e0f24fc695b2edd
gornostay25 <volodia.palamar250405@gmail.com>
2018-04-15 17:12:50 +0300

    add

--- install.py
+++ install.py
@@ -8,11 +8,11 @@ sp.call('rm -fr requirements.txt',shell=True)
 sp.call('rm -fr install.py',shell=True)
 os.system('rm -fr README.md .git/ ')
 print('install finished!\n')
-print('start program? (y or n)')
-a = input().upper()
+a = input('start program? (y or n): ')
+a.upper()
 if a == 'Y':
   os.system('python3 camscan.py')
 elif a == 'N':
   exit()
-exit()
+print('123')
 
