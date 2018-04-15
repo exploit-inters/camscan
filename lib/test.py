0a5b47d9360d277633f263f7a320285513d5a03c
Gornostay <36516130+gornostay25@users.noreply.github.com>
2018-04-15 18:28:13 +0300

    Update test.py

--- lib/test.py
+++ lib/test.py
@@ -1,2 +1,15 @@
+import socket
+import subprocess as sp
+import os
+import sys
+import random
+import time as t
+try:
+	from netaddr import *
+	import requests
+except ModuleNotFoundError:
+	print("будласка встановіть бібліотеки\npip install -r requirements.txt\n")
+	exit()
+
 def test(t):
 	print(t)
